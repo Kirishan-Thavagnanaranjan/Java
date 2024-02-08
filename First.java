111c6e4d7fa89afa7753b41a674b6b4d623eab8d
Kirishan <150703638+Kirishan-Thavagnanaranjan@users.noreply.github.com>
2024-02-09 02:15:41 +0530

    Update First.java
    assign variable

--- First.java
+++ First.java
@@ -2,7 +2,10 @@ package trainned;
 
 public class First {
 	public static void main (String args[]) {
-		System.out.print("Hello world");
+		System.out.println("Hello world");
+		int Ticketprice;
+		Ticketprice = 800;
+		System.out.println(Ticketprice);
 	}
 
 }
