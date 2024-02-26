f250ee8269cca49ec231cb3344fd1bb09a1078c0
Kirishan <150703638+Kirishan-Thavagnanaranjan@users.noreply.github.com>
2024-02-26 21:43:51 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,13 @@
+package Second;
+
+public class Second {
+	public static void main(String args[])
+	{
+		System.out.println("Hello world");
+		int Ticketprice;
+		Ticketprice=800;
+		System.out.print(Ticketprice);
+	}
+	
+
+}
