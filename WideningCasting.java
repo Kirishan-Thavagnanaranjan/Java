a68063b6062cb2da3de7a9f8b82bc15ad91c86ad
Kirishan <150703638+Kirishan-Thavagnanaranjan@users.noreply.github.com>
2024-03-13 02:31:20 +0530

    Add files via upload

---
+++ WideningCasting.java
@@ -0,0 +1,11 @@
+public class WideningCasting
+{
+	public static void main(String[] args)
+	{
+	 int myint = 5;
+	 double mydouble = myint; //widening casting 
+
+	 System.out.println(myint);
+	 System.out.println(mydouble);
+	}
+}
