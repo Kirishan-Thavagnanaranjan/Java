ab60da7a4b678665c4d59d60b76c39f7dfbd39c5
Kirishan <150703638+Kirishan-Thavagnanaranjan@users.noreply.github.com>
2024-03-13 02:36:34 +0530

    Add files via upload

---
+++ NarrowingCasting.java
@@ -0,0 +1,11 @@
+public class NarrowingCasting
+{
+	public static void main(String[] args)
+	{
+		double MyDouble = 5.5;
+		int MyInt = (int) MyDouble;
+
+		System.out.println(MyDouble);
+		System.out.println(MyInt);
+	}
+}
