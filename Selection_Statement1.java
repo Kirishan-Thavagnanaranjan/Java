8e85e1e66d996a3dae52423b82d0729d24b37f73
Kirishan <150703638+Kirishan-Thavagnanaranjan@users.noreply.github.com>
2024-03-16 14:50:41 +0530

    Add files via upload

---
+++ Selection_Statement1.java
@@ -0,0 +1,17 @@
+public class Selection_Statement1
+{
+	public static void main(String[] args)
+	{
+		int a=5;
+		int b=10;
+
+		if (a>b)
+		{
+			System.out.println("a is Greater than b");
+		}
+		else 
+		{
+			System.out.println("b is greater than a");
+		}
+	}
+}
